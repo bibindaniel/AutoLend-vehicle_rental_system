c56d7920a44c61620b7bff41d7846bf2aac6590d
bibindaniel <bibindaniel10@gmail.com>
2023-02-21 16:58:36 +0530

    added verify user with validation

--- adminpage.php
+++ adminpage.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autolend</title>
     <link rel="icon" href="Images/Logo.png">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
 </head>
 <body>
     
